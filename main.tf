eacfe80543bc9059097ead9e07f25670835524fe
Mahesh-Gontla <115606905+Mahesh-Gontla@users.noreply.github.com>
2024-07-24 10:03:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,25 @@ provider "aws" {
 # Create a VPC
 resource "aws_instance" "web_server" {
   ami           = "ami-04a81a99f5ec58529" # Replace with the desired AMI ID
-  instance_type = t2.micro
+  instance_type = var.ec2_instance_type
   tags = {
     Name = "web-server"
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
